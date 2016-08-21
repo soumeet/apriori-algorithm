d9d37eeccbdda0a378c94b782e772527589eeab4
soumeet <soumeet.basak@gmail.com>
2016-08-22 01:26:16 +0530

    traversing itemset and support calculations

--- apriori.py
+++ apriori.py
@@ -2,8 +2,16 @@ import sys
 import csv
 
 DATASET=sys.argv[1]
-
+no_of_transaction=0
+min_support=0.0
+item_support=[]
+item_count=[0]*1000
 with open(DATASET, newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in spamreader:
-    	print(row)
+    itemset = csv.reader(csvfile)
+    for row in itemset:
+    	for i in row:
+    		item_count[int(i)]+=1
+    	no_of_transaction+=1
+for i in range(0, no_of_transaction):
+	item_support.append(item_count[i]/no_of_transaction)
+print(item_support)
