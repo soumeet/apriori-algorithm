377ccf2b1b14865a6f3309d193fdf3f719f4d9f0
soumeet <soumeet.basak@gmail.com>
2016-08-22 00:36:55 +0530

    column element

--- test1.py
+++ test1.py
@@ -1,5 +1,11 @@
 import csv
+count=[0]
+for i in range(0, 1000):
+	count.append(0) 
+print (count)
 with open('T10I4D100K.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    spamreader = csv.reader(csvfile)
+    mx=0
     for row in spamreader:
-    	print(row)
+    	mx=max(row)
+    print("max: ", mx)
