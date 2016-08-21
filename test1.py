9a65ed41aef6e283c1e8c21045d7515e5e46eada
soumeet <soumeet.basak@gmail.com>
2016-08-22 01:02:43 +0530

    support calculation

--- test1.py
+++ test1.py
@@ -1,9 +1,15 @@
 import csv
-count=[0]*1000 
-print (count)
+count=[0]*1000
+support=[0.0]*1000
+LENGTH=0
 with open('T10I4D100K.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile)
     mx=0
     for row in spamreader:
-    	mx=max(row)
-    print("max: ", mx)
+    	for i in row:
+    		count[int(i)]=count[int(i)]+1
+    	LENGTH=LENGTH+1
+for i in range(0, LENGTH):
+	support[i]=count[i]/LENGTH
+min_support=min(support)
+print(min_support)
