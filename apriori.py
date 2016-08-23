c7a874e84e09c04a0091d067b37ebdfcf644d6f1
soumeet <soumeet.basak@gmail.com>
2016-08-24 03:30:28 +0530

    frequent 2-itemset generated

--- apriori.py
+++ apriori.py
@@ -9,7 +9,11 @@ itemset=[]
 itemset_support={}
 itemset_count={}
 frequent_1itemset=[]
+itemset2=[]
+itemset2_support={}
+itemset2_count={}
 frequent_2itemset=[]
+itemset3=[]
 def read_transactions():
 	with open(dataset, newline='') as csvfile:
             spamreader = csv.reader(csvfile)
@@ -25,11 +29,9 @@ def read_transactions():
                     transaction_row.append(item)
                 transactions.append(transaction_row)
 
-def generate_support():
+def generate_1itemset():
 	for item in itemset:
 		itemset_support[item]=float(itemset_count[item]/no_of_transaction)
-
-def generate_1itemset():
 	for item in itemset:
 		if itemset_support[item] >= min_support:
 			frequent_1itemset.append(item)
@@ -37,15 +39,23 @@ def generate_1itemset():
 def generate_2itemset():
 	for item1 in frequent_1itemset:
 		for item2 in frequent_1itemset:
-			if not item1 == item2:
-				if [item2, item1] not in frequent_2itemset:
-					frequent_2itemset.append([item1, item2])
+			if not item1==item2:
+				if [item2,item1] not in itemset2:
+					itemset2.append([item1,item2])
+					itemset2_count[item1,item2]=0
+	for items in itemset2:
+		for row in transactions:
+			if set(items).issubset(row):
+					itemset2_count[items[0],items[1]]+=1
+	for items in itemset2:
+		itemset2_support[items[0],items[1]]=float(itemset2_count[items[0],items[1]]/no_of_transaction)
+	for items in itemset2:
+		if itemset2_support[items[0],items[1]] >= min_support:
+			frequent_2itemset.append(items)
 
 read_transactions()
 no_of_transaction=len(transactions)
 itemset.sort()
-generate_support()
 generate_1itemset()
 generate_2itemset()
-print(frequent_1itemset)
 print(frequent_2itemset)
