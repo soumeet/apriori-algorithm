4f89af9220aa7be4c0a139064b9626138b04ceeb
sayan-syn94 <devillikeme@gmail.com>
2016-08-25 01:06:52 +0530

    partition algorithm implemented
    IndexOutOfIndex Exception

--- test5.py
+++ test5.py
@@ -1,7 +1,8 @@
 import csv
 transaction=[]
 transaction_part=[]
-item=[]
+item=[[0,[]]]
+c2=1
 def transactions():
 	count=[]
 	with open('T10I4D100K.csv', 'rb') as csvfile:
@@ -14,11 +15,24 @@ def transactions():
     			count.append(i)
     		transaction.append(count)
     		count =[]
+
 def partition():
  	size = []
- 	c=0
- 	for i in range(0,len(transaction)):
- 		size[i] = transaction[i].count()
- 		for j in range(0,len(transaction)):
- 			if size[i] == size[j]:
- 				transaction_part[0]=size[i]
+ 	c = 0
+ 	for i in range(len(transaction)):
+         size.append(len(transaction[0]))
+         for j in range(i+1,len(transaction)):
+             if size[i]== len(transaction[j]):
+                 transaction_part[c].append(size[i])
+                 if transaction[i] not in transaction_part[c]:
+                     transaction_part[c][c1].append(transaction[i])
+                 transaction_part[c].append(list(transaction[j]))
+                 c=c+1
+
+transactions()
+partition()
+print len(transaction[0])
+
+
+
+
